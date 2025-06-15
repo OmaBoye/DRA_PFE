37da67c4c0bcf220966451d84b36fe148f2090f6
OmaBoye <sonyoumama@gmail.com>
2025-06-15 14:41:08 +0000

    err

--- SGL/analytics/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TurnaroundTimeReport, sample_throughput
-
-urlpatterns = [
-    path('analytics/turnaround/', TurnaroundTimeReport.as_view(), name='turnaround_report'),
-    path('analytics/throughput/', sample_throughput, name='throughput_report'),
-]
--- SGL/integrations/management/commands/start_h17.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from integrations.mllp_listener import HL7Handler
-
-class Command(BaseCommand):
-    help = 'Starts HL7 MLLP listener'
-
-    def handle(self, *args, **options):
-        HL7Handler().start()
